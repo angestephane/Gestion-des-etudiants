a588903c8b47b896d48155aa071e45902305e628
stephane boguhe <boguhestephane97@gmail.com>
2021-11-06 02:00:53 +0100

    Initial commit

---
+++ src/main/java/com/steph/gestiondesetudiants/repository/EtudiantRepository.java
@@ -0,0 +1,11 @@
+package com.steph.gestiondesetudiants.repository;
+
+import com.steph.gestiondesetudiants.entity.Etudiant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EtudiantRepository extends JpaRepository<Long, Etudiant> {
+
+
+}
