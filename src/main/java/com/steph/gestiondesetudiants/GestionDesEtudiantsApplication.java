ccbbea2eeba9caf391596ae6cc4d98b5a6885e97
stephane boguhe <boguhestephane97@gmail.com>
2021-11-07 01:00:40 +0100

    initial commit

--- src/main/java/com/steph/gestiondesetudiants/GestionDesEtudiantsApplication.java
+++ src/main/java/com/steph/gestiondesetudiants/GestionDesEtudiantsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestionDesEtudiantsApplication {
+public class GestionDesEtudiantsApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(GestionDesEtudiantsApplication.class, args);
